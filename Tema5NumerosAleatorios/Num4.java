0200c3f0c7a0cb54b15caf230b2ad309daa82516
Angela Ruiz Rivas <148638390+Anpiarur@users.noreply.github.com>
2024-01-26 13:47:33 +0100

    Keep learning

---
+++ Tema5NumerosAleatorios/Num4.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package AprendeConJava.Tema5NumerosAleatorios;
+
+/**
+ *Ejercicio 4
+Muestra 20 números enteros aleatorios entre 0 y 10 (ambos incluidos) separa-
+dos por espacios.
+ * @author angelaruiz
+ */
+public class Num4 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 20; i++) {
+            int numero =(int)(Math.random()*11);
+            System.out.print(numero+" ");
+            
+        }
+    }
+ 
+}
