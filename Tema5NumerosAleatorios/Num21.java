4d055cadf0a5b30efe3c78a57ff8e11c85f05ce0
Angela Ruiz Rivas <148638390+Anpiarur@users.noreply.github.com>
2024-02-03 23:10:56 +0100

    Add files via upload

---
+++ Tema5NumerosAleatorios/Num21.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package AprendeConJava.Tema5NumerosAleatorios;
+
+/**
+ * Ejercicio 21 Realiza un programa que genere una secuencia de cinco monedas de
+ * curso legal lanzadas al aire. Las monedas disponibles son de 1 céntimo, 2
+ * céntimos, 5 céntimos, 10 céntimos, 20 céntimos, 50 céntimos, 1 euro y 2
+ * euros. Las dos posiciones posibles son cara y cruz. Ejemplo: 2 céntimos -
+ * cara 20 céntimos - cruz 50 céntimos - cruz 1 euro - cruz 2 euros - cara
+ *
+ * @author angelaruiz
+ */
+public class Num21 {
+
+    public static void main(String[] args) {
+
+        String moneda = " ";
+        String cara = " ";
+        for (int i = 0; i <= 5; i++) {
+            int valorCara = (int) (Math.random() * 2);
+            switch (valorCara) {
+                case 0:
+                    cara = "cara";
+                    break;
+                case 1:
+                    cara = "cruz";
+                    break;
+            }
+            switch (i) {
+                case 0:
+                    moneda = "2 céntimos";
+                    break;
+                case 1:
+                    moneda = "10 céntimos";
+                    break;
+                case 2:
+                    moneda = "20 céntimos";
+                    break;
+                case 3:
+                    moneda = "50 céntimos";
+                    break;
+                case 4:
+                    moneda = "1 euro";
+                    break;
+                case 5:
+                    moneda = "2 euros";
+                    break;
+            }
+            System.out.println(moneda + "-" + cara);
+        }
+    }
+}
