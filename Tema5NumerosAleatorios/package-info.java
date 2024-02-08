21615c3171a87a0567d31ba6c8624dfa49a58582
Angela Ruiz <148638390+Anpiarur@users.noreply.github.com>
2024-02-08 18:23:27 +0100

    add 2024-02-08

--- Tema5NumerosAleatorios/package-info.java
+++ Tema5NumerosAleatorios/package-info.java
@@ -17,5 +17,130 @@ String.valueOf(n).
 Ejercicio 3
 Igual que el ejercicio anterior pero con la baraja española. Se utilizará la baraja
 de 40 cartas: 2, 3, 4, 5, 6, 7, sota, caballo, rey y as.
+Ejercicio 4
+Muestra 20 números enteros aleatorios entre 0 y 10 (ambos incluidos) separa-
+dos por espacios.
+Ejercicio 5
+Muestra 50 números enteros aleatorios entre 100 y 199 (ambos incluidos)
+separados por espacios. Muestra también el máximo, el mínimo y la media
+de esos números.
+Ejercicio 6
+Escribe un programa que piense un número al azar entre 0 y 100. El usuario de-
+be adivinarlo y tiene para ello 5 oportunidades. Después de cada intento fallido,
+el programa dirá cuántas oportunidades quedan y si el número introducido es
+menor o mayor que el número secreto.
+Ejercicio 7
+Escribe un programa que muestre tres apuestas de la quiniela en tres columnas
+para los 14 partidos y el pleno al quince (15 filas).
+Ejercicio 8
+Modifica el programa anterior para que la probabilidad de que salga un 1 sea
+de 1/2, la probabilidad de que salga x sea de 1/3 y la probabilidad de que salga
+2 sea de 1/6. Pista: 1/2 = 3/6 y 1/3 = 2/6.
+Ejercicio 9
+Realiza un programa que vaya generando números aleatorios pares entre 0
+y 100 y que no termine de generar números hasta que no saque el 24. El
+programa deberá decir al final cuántos números se han generado.
+Ejercicio 9
+Realiza un programa que vaya generando números aleatorios pares entre 0
+y 100 y que no termine de generar números hasta que no saque el 24. El
+programa deberá decir al final cuántos números se han generado.
+Ejercicio 10
+Realiza un programa que pinte por pantalla diez líneas formadas por caracteres.
+El carácter con el que se pinta cada línea se elige de forma aleatoria entre uno
+de los siguientes: *, -, =, ., |, @. Las líneas deben tener una longitud aleatoria
+entre 1 y 40 caracteres.
+Ejercicio 11
+Escribe un programa que muestre 20 notas generadas al azar. Las notas deben
+aparecer de la forma: suspenso, suficiente, bien, notable o sobresaliente. Al
+final aparecerá el número de suspensos, el número de suficientes, el número
+de bienes, etc.
+Ejercicio 12
+Realiza un programa que llene la pantalla de caracteres aleatorios (a lo Matrix)
+con el código ascii entre el 32 y el 126. Puedes hacer casting con (char) para
+convertir un entero en un carácter.
+Ejercicio 13
+Escribe un programa que simule la tirada de dos dados. El programa deberá
+continuar tirando los dados una y otra vez hasta que en alguna tirada los dos
+dados tengan el mismo valor.
+Ejercicio 14
+Realiza un programa que haga justo lo contrario a lo que hace el ejercicio 6. El
+programa intentará adivinar el número que estás pensando - un número entre 0
+y 100 - teniendo para ello 5 oportunidades. En cada intento fallido, el programa
+debe preguntar si el número que estás pensando es mayor o menor que el que
+te acaba de decir.
+Realiza un generador de melodía con las siguientes condiciones:
+a) Las notas deben estar generadas al azar. Las 7 notas son do, re, mi, fa, sol,
+la y si.
+b) Una melodía está formada por un número aleatorio de notas mayor o igual
+a 4, menor o igual a 28 y siempre múltiplo de 4 (4, 8, 12...).
+c) Cada grupo de 4 notas será un compás y estará separado del siguiente
+compás mediante la barra vertical “|” (Alt + 1). El final de la melodía se marca
+con dos barras.
+d) La última nota de la melodía debe coincidir con la primera.
+Ejercicio 16
+Realiza un simulador de máquina tragaperras simplificada que cumpla los
+siguientes requisitos:
+a) El ordenador mostrará una tirada que consiste en mostrar 3 figuras. Hay 5
+figuras posibles: corazón, diamante, herradura, campana y limón.
+b) Si las tres figuras son diferentes se debe mostrar el mensaje “Lo siento, ha
+perdido”.
+c) Si hay dos figuras iguales y una diferente se debe mostrar el mensaje “Bien,
+ha recuperado su moneda”.
+d) Si las tres figuras son iguales se debe mostrar “Enhorabuena, ha ganado
+10 monedas”.
+Ejercicio 17
+Realiza un programa que pinte por pantalla una pecera con un pececito dentro.
+Se debe pedir al usuario el ancho y el alto de la pecera, que como mínimo
+serán de 4 unidades. No hay que comprobar que los datos se introducen
+correctamente; podemos suponer que el usuario los introduce bien. Dentro de
+la pecera hay que colocar de forma aleatoria un pececito, que puede estar
+situado en cualquiera de las posiciones que quedan en el hueco que forma el
+rectángulo.
+Ejercicio 18
+Sinestesio y Casilda van a pintar los tres dormitorios de su casa, quieren
+sustituir el color blanco por colores más alegres. Realiza un programa que
+genere de forma aleatoria una secuencia de tres colores aleatorios (uno para
+cada dormitorio) de tal forma que no se repita ninguno. Los colores entre los que
+debe elegir el programa son los siguientes: rojo, azul, verde, amarillo, violeta y
+naranja.
+Ejercicio 19
+Escribe un programa que muestre 50 números enteros aleatorios comprendidos
+entre el -100 y el 200 ambos incluidos y separados por espacios. Muestra luego
+el máximo de los pares el mínimo de los impares y la media de todos los
+números generados.
+Ejercicio 20
+Realiza un programa que pinte por pantalla una cuba con cierta cantidad de
+agua. La capacidad será indicada por el usuario. La cuba se llenará con una
+cantidad aleatoria de agua que puede ir entre 0 y la capacidad máxima que
+pueda admitir. El ancho de la cuba no varía.
+ *Ejercicio 21
+Realiza un programa que genere una secuencia de cinco monedas de curso
+legal lanzadas al aire. Las monedas disponibles son de 1 céntimo, 2 céntimos,
+5 céntimos, 10 céntimos, 20 céntimos, 50 céntimos, 1 euro y 2 euros. Las dos
+posiciones posibles son cara y cruz.
+ *Ejercicio 22
+Realiza un programa que pinte por pantalla una serpiente con un “serpenteo”
+aleatorio. La cabeza se representará con el carácter @ y se debe colocar
+exactamente en la posición 13 (con 12 espacios delante). A partir de ahí,
+el cuerpo irá serpenteando de la siguiente manera: se generará de forma
+aleatoria un valor entre tres posibles que hará que el siguiente carácter se
+coloque una posición a la izquierda del anterior, alineado con el anterior o una
+posición a la derecha del anterior. La longitud de la serpiente se pedirá por
+teclado y se supone que el usuario introducirá un dato correcto.
+ *Ejercicio 23
+Las caras de un dado de poker tienen las siguientes figuras: As, K, Q, J, 7 y 8.
+Escribe un programa que genere de forma aleatoria la tirada de cinco dados.
+Ejercicio 24 Escribe un programa que, dado un número introducido por teclado,
+ * elija al azar uno de sus dígitos
+Ejercicio 25
+Escribe un programa que muestre por pantalla 100 números enteros separados
+por un espacio. Los números deben estar generados de forma aleatoria en
+un rango entre 10 y 200 ambos incluidos. Los primos deben aparecer entre
+almohadillas (p. ej. #19#) y los múltiplos de 5 entre corchetes (p. ej. [25]).
+Ejercicio 26
+Realiza un programa que pinte una tableta de turrón con un bocado realizado de
+forma aleatoria. El ancho y el alto de la tableta se pide por teclado. El bocado se
+da alrededor del turrón, obviamente no se puede dar un bocado por en medio
+de la tableta.
 * 
 */
