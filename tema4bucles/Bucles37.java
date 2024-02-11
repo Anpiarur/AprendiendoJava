7bd7869fca640743fcb4fb61d29b9a7dca89c71b
anpiarur <angelaruizdev@gmail.com>
2024-02-11 16:21:27 +0100

    Undone

--- tema4bucles/Bucles37.java
+++ tema4bucles/Bucles37.java
@@ -1,6 +1,6 @@
 package tema4bucles;
 
-import java.util.Scanner;
+
 
 /**
  * Ejercicio 37 Realiza un conversor del sistema decimal al sistema de
@@ -13,46 +13,6 @@ import java.util.Scanner;
 public class Bucles37 {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Conversor de sistema decimal al sistema de palotes");
-        System.out.println("Introduzca un número entero positivo:");
-        int numero = sc.nextInt();
-        sc.close();
-
-        if (numero < 0) {
-            System.out.println("El número debe ser un entero no negativo.");
-        } else {
-            int temp = numero;
-            int invertido = 0;
-
-            // Manejo especial para el caso de 0
-            String palote;
-            if (temp == 0) {
-                palote = "-";
-            } else {
-                palote = "|";
-            }
-
-            // Convertir el número a palotes
-            while (temp > 0) {
-                int digito = temp % 10;
-                invertido = invertido * 10 + digito;
-                temp /= 10;
-            }
-
-            // Imprimir palotes
-            while (invertido > 0) {
-                int digito2 = invertido % 10;
-                for (int i = 0; i < digito2; i++) {
-                    System.out.print("|");
-                }
-                if (invertido / 10 > 0) {
-                    System.out.print("-");
-                }
-                invertido /= 10;
-            }
-
-            System.out.println("\nEs el " + numero + " pasado de sistema  decimal a sistema de palotes.");
-        }
+       
     }
 }
