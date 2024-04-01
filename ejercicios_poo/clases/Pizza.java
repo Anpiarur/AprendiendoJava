e54dad5a91938fc1f600f2382e43d0db2b8a089b
anpiarur <angelaruizdev@gmail.com>
2024-04-01 14:51:08 +0200

    fixed

--- ejercicios_poo/clases/Pizza.java
+++ ejercicios_poo/clases/Pizza.java
@@ -1,38 +1,39 @@
 package ejercicios_poo.clases;
 
 public class Pizza {
-private String tipo;
-private String tamaño;
-private static int pedidas=0;
-private static int servidas = 0;
-private static int pendientesServir;
+    private String tipo;
+    private String tamaño;
+    private boolean pedida; // Estado de la pizza: pedida o servida
+    private static int totalPedidas = 0; // Contador de pizzas pedidas
+    private static int totalServidas = 0; // Contador de pizzas servidas
 
-public Pizza (String tipo, String tamaño){
-    this.tipo=tipo;
-    this.tamaño=tamaño;
-    pedidas++;
-}
-
-
-
-public  static void sirve(){
-    if (pedidas > 0) {
-        pendientesServir=pedidas;
-        servidas++; 
-        pendientesServir--;} 
+    public Pizza(String tipo, String tamaño) {
+        this.tipo = tipo;
+        this.tamaño = tamaño;
+        this.pedida = true; // Una pizza nueva siempre se inicializa como "pedida"
+        totalPedidas++; // Incrementamos el contador de pizzas pedidas
     }
 
-    public static String getTotalPedidas() {
-            return "Las pizzas pedidas son :"+pedidas;
+    public void sirve() {
+        if (pedida) {
+            System.out.println("pizza " + tipo + " " + tamaño + ", servida");
+            pedida = false; // Cambiamos el estado de la pizza a "servida"
+            totalServidas++; // Incrementamos el contador de pizzas servidas
+        } else {
+            System.out.println("esa pizza ya se ha servido");
+        }
     }
 
-    public static String getPizzeriaPedidos(){
-        return "Las pizzas pedidas en total son "+ pedidas+
-        "Las pizzas pendientes de servir son "+ pendientesServir+
-        "Las pizzas ya servidas son "+servidas;
+    public static int getTotalPedidas() {
+        return totalPedidas;
     }
 
-    public static String getTotalServidas() {
-        return "Las pizzas servidas son :"+servidas;
+
+    public static int getTotalServidas() {
+        return totalServidas;
+    }
+    public String toString() {
+        String estado = pedida ? "pedida" : "servida";
+        return "pizza " + tipo + " " + tamaño + ", " + estado;
     }
 }
